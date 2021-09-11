d4b9ac57e0cd2e6445290766a370ff9de2ac88aa
Aabir Datta <dattaaabir13@gmail.com>
2021-09-12 01:10:26 +0530

    Create DataType.java

---
+++ Basics/DataType.java
@@ -0,0 +1,27 @@
+//Code to demonstate the varying data types
+public class DataType 
+{
+    public static void main(String[] args)
+    {
+        //Character data type
+        char a = 'A';
+        //Integer data type
+        int b = 121;
+        //Byte and short data type for smaller values
+        byte c = 1;
+        short d = 11;
+        //Float data type for smaller decimal places
+        float e = 3.14f;
+        //Double data type for larger decimal places
+        double f =  3.141592653589;
+        
+        //Printing the values
+        System.out.println("char: "+a);
+        System.out.println("int: "+b);
+        System.out.println("byte: "+c);
+        System.out.println("short: "+d);
+        System.out.println("float: "+e);
+        System.out.println("double: "+f);
+    }
+    
+}
