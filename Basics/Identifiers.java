cd02de242e5ad5829200c1fbeffeb73bfa231c46
Aabir Datta <dattaaabir13@gmail.com>
2021-09-12 01:08:58 +0530

    Create Identifiers.java

---
+++ Basics/Identifiers.java
@@ -0,0 +1,16 @@
+public class Identifiers 
+{
+    public static void main(String[] args)
+    {
+        int a = 7;
+        System.out.println(a);
+    }
+}
+
+/*In the above code we shall demarcate the 5 identifiers namely:
+1. Identifiers: class name
+2. main: method name
+3. String: predefined class name
+4. args: variable name
+5. a: variable name
+*/
