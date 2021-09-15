f8513acdd4d7961ac071a415213ed30a6c7c12c6
Aabir Datta <dattaaabir13@gmail.com>
2021-09-15 12:18:54 +0530

    Create Boolean.java

---
+++ Basics/Boolean.java
@@ -0,0 +1,13 @@
+//Basic program to understand Boolean data type
+public class Boolean 
+{
+    public static void main(String[] args)
+    {
+        boolean b = true;
+        if(b == true)
+        {
+            System.out.println("Aabir!");
+        }
+    }    
+
+}
