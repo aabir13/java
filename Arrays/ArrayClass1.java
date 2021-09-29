f224651a56845eeec9b23dee701790e4ff1ad008
Aabir Datta <dattaaabir13@gmail.com>
2021-09-29 21:21:52 +0530

    Create ArrayClass1.java

---
+++ Arrays/ArrayClass1.java
@@ -0,0 +1,12 @@
+//Code to demonstrate Arrays Class via asList() function
+import java.util.*;
+public class ArrayClass1 
+{
+    public static void main(String[] args)
+    {
+        int ar[] = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Integer Array as List: "+Arrays.asList(ar));
+
+    }
+    
+}
