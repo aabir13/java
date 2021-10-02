8729f22201e8c0c043696ad66f341badc8020fc5
Aabir Datta <dattaaabir13@gmail.com>
2021-10-02 16:28:09 +0530

    Create ArrayClass11.java

---
+++ Arrays/ArrayClass11.java
@@ -0,0 +1,12 @@
+//Code to demonstrate sort() method of Arrays Class
+//Sort functions on quick sort algorithm
+import java.util.*;
+public class ArrayClass11 
+{
+    public static void main(String[] args)
+    {
+        int ar[]={22,15,10,35,20};
+        Arrays.sort(ar);
+        System.out.println("Sorted Array: "+Arrays.toString(ar));
+    }
+}
