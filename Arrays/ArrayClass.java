3d39a46c7bec27f29e950f75c2f74cd3914bc3ca
Aabir Datta <dattaaabir13@gmail.com>
2021-10-02 21:08:28 +0530

    Create ArrayClass.java

---
+++ Arrays/ArrayClass.java
@@ -0,0 +1,12 @@
+//Code to demonstrate toString() method of Array Class to print Arrays with ease
+import java.util.*;
+public class ArrayClass 
+{
+    public static void main(String[] args)
+    {
+        int ar[]={10,20,15,22,35};
+        System.out.println("Integer Array: "+Arrays.toString(ar));
+        String arr[]={"Aabir","Anushka","Simba","Gauri"};
+        System.out.println("Integer Array: "+Arrays.toString(arr));
+    }
+}
