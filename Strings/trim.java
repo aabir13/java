6177d3a8cad71f5b52161ace6efa9a6ecf6dad85
Aabir Datta <dattaaabir13@gmail.com>
2021-10-03 20:50:38 +0530

    Create trim.java

---
+++ Strings/trim.java
@@ -0,0 +1,9 @@
+//Code to understand the trim() method of String class which removes the additional white spaces at the start and end of the String
+public class trim 
+{
+    public static void main(String[] args)
+    {
+        String str = "  aabir anushka   ";
+        System.out.println(str.trim());
+    }
+}
