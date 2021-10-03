f98ea3c1c10c2c54a3fa67ebee2033a65985ba71
Aabir Datta <dattaaabir13@gmail.com>
2021-10-03 20:54:58 +0530

    Create CaseChanging.java

---
+++ Strings/CaseChanging.java
@@ -0,0 +1,10 @@
+//Code to demonstrate changing cases using String class functions
+public class CaseChanging 
+{
+    public static void main(String[] args)
+    {
+        String s1="anushka";
+        String s2="AABIR";
+        System.out.println(s1.toUpperCase()+s2.toLowerCase());
+    }  
+}
