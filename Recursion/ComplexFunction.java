3049bbdc06f5b19f2e745e98d69fc7dd401714f2
Aabir Datta <dattaaabir13@gmail.com>
2022-04-30 03:42:59 +0530

    Create ComplexFunction.java

---
+++ Recursion/ComplexFunction.java
@@ -0,0 +1,10 @@
+class ComplexFunction {
+    int calculateFactorial (int n) {
+        if (n==1) {
+            return 1;
+        }
+        else {
+            return (n*calculateFactorial(n-1));
+        }
+    }
+}
