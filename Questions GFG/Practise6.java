7586fd72b640dbbcd4b27450fd63884d8be0de08
Aabir Datta <dattaaabir13@gmail.com>
2022-05-03 22:24:05 +0530

    Create Practise6.java

---
+++ Questions GFG/Practise6.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Practise6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int num = n;
+        int sum=0;
+        while(num>0) {
+            int temp = num%10;
+            sum += (int)Math.pow(temp,3);
+            num /= 10;
+        }
+        if(sum == n) {
+            System.out.println("Armstrong Number");
+        }
+        else {
+            System.out.println("Not an Armstrong Number");
+        }
+        sc.close();
+    }
+}
